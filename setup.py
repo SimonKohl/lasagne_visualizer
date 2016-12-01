714851a7dcd3f873256b36efc8343dabe6d94338
Simon Kohl <simon.kohl@dkfz-heidelberg.de>
2016-12-01 18:16:27 +0100

    only use py 2.7 and 3.4 with matplotlib 1.5.3

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4'
     ],
     test_suite='tests',
     tests_require=test_requirements
