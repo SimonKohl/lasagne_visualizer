6ed07106d9ec4d2b547ffebdf9ea1ad51a3946dd
Simon Kohl <simon.kohl@dkfz-heidelberg.de>
2016-12-01 18:29:59 +0100

    drop python 3.4 altogether

--- setup.py
+++ setup.py
@@ -61,8 +61,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 2.7'
     ],
     test_suite='tests',
     tests_require=test_requirements
