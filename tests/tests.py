12dde9f29ce2855aef4c159d7d80f3759b945f72
Simon Kohl <simon.kohl@dkfz-heidelberg.de>
2016-12-01 17:58:10 +0100

    changed display port

--- tests/tests.py
+++ tests/tests.py
@@ -10,9 +10,9 @@ Tests for `lasagne_visualizer` module.
 
 import unittest
 
-# import sys, os
-# myPath = os.path.dirname(os.path.abspath(__file__))
-# sys.path.insert(0, myPath + '/../')
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
 from lasagne_visualizer import lasagne_visualizer
 from lasagne.layers import InputLayer, ReshapeLayer, Conv2DLayer, DenseLayer
 from lasagne.nonlinearities import softmax
