5ef7358fa3f4cea7bf762b816cd170b0f88683ce
Simon Kohl <simon.kohl@dkfz-heidelberg.de>
2016-12-02 08:39:31 +0100

    dropping a couple requirements

--- setup.py
+++ setup.py
@@ -26,14 +26,18 @@ with open('HISTORY.rst') as history_file:
 # #
 # requirements = test_requirements
 
+# requirements = [
+# "alabaster==0.7.9", "argh==0.26.2", "arrow==0.10.0", "Babel==2.3.4", "binaryornot==0.4.0", "cffi==1.9.1",\
+# "chardet==2.3.0", "click==6.6", "cycler==0.10.0", "docutils==0.12",\
+# "enum34==1.1.6", "future==0.16.0", "idna==2.1", "imagesize==0.7.1", "ipaddress==1.0.17", "Jinja2==2.8", "jinja2-time==0.2.0",\
+# "Lasagne==0.1", "MarkupSafe==0.23", "matplotlib==1.5.3", "mccabe==0.5.2", "numpy==1.11.2", "pandas==0.19.1", "pathtools==0.1.2", "pluggy==0.3.1",\
+# "poyo==0.4.0", "py==1.4.31", "pyasn1==0.1.9", "pycodestyle==2.0.0", "pycparser==2.17", "pyflakes==1.2.3", "Pygments==2.1.3", "pyparsing==2.1.10",\
+# "python-dateutil==2.6.0", "pytz==2016.7", "six==1.10.0", "snowballstemmer==1.2.1", "Sphinx==1.4.8", "Theano==0.8.2",\
+# "virtualenv==15.1.0", "whichcraft==0.4.0"
+# ]
+
 requirements = [
-"alabaster==0.7.9", "argh==0.26.2", "arrow==0.10.0", "Babel==2.3.4", "binaryornot==0.4.0", "cffi==1.9.1",\
-"chardet==2.3.0", "click==6.6", "cycler==0.10.0", "docutils==0.12",\
-"enum34==1.1.6", "future==0.16.0", "idna==2.1", "imagesize==0.7.1", "ipaddress==1.0.17", "Jinja2==2.8", "jinja2-time==0.2.0",\
-"Lasagne==0.1", "MarkupSafe==0.23", "matplotlib==1.5.3", "mccabe==0.5.2", "numpy==1.11.2", "pandas==0.19.1", "pathtools==0.1.2", "pluggy==0.3.1",\
-"poyo==0.4.0", "py==1.4.31", "pyasn1==0.1.9", "pycodestyle==2.0.0", "pycparser==2.17", "pyflakes==1.2.3", "Pygments==2.1.3", "pyparsing==2.1.10",\
-"python-dateutil==2.6.0", "pytz==2016.7", "six==1.10.0", "snowballstemmer==1.2.1", "Sphinx==1.4.8", "Theano==0.8.2",\
-"virtualenv==15.1.0", "whichcraft==0.4.0"
+"Lasagne==0.1", "matplotlib==1.5.3", "numpy==1.11.2", "pandas==0.19.1", "py==1.4.31", "python-dateutil==2.6.0", "Theano==0.8.2"
 ]
 
 test_requirements = requirements
